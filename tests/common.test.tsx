56a7a6c964034b9d7388dbd62de9e88c7c4cac3a
Sergey Timokhin <s_timokhin@wargaming.net>
2024-02-07 18:23:04 +0200

    ns

--- tests/common.test.tsx
+++ tests/common.test.tsx
@@ -1,9 +1,8 @@
-import * as React from 'react'
 import { render } from '@testing-library/react'
 
 import 'jest-canvas-mock'
 
-import App from '../src/App'
+import { App } from '../src/components/App'
 
 describe('common render', () => {
   it('renders without crushing', () => {
